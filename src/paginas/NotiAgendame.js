5262efe9822109b84612aad2ddf8fdfba7b0a6ff
Javier <javiteje92@gmail.com>
2023-04-18 03:49:30 -0300

    noticias sin estilos

---
+++ src/paginas/NotiAgendame.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const NotiAgendame = () => {
+    return (
+        <div>
+            <h1 class="entry-title entry--item h2">Concientización. Inprojuy lanzó concursos sobre Juego Responsable</h1>
+
+            <p>El objetivo del Instituto Provincial de Juegos de Azar (Inprojuy) es promover el Juego Responsable, a través de la convocatoria a dos concursos con premios.</p>
+
+
+            <img src=".\noticias\img\notiagendame.webp" />
+
+
+            <p>Con el objetivo que la población adquiera conocimientos acerca del Juego Responsable, el Instituto Provincial de Juegos de Azar (Inprojuy), a través del Programa Juga x Jugar, lanzó los concursos “Agendame” y la segunda edición del Concurso Literario, en los que se podrá participar hasta el 17 de abril, abiertos a toda la comunidad.</p>
+
+
+
+            <p>Para participar del certamen denominado “Agendame”, los interesados deben enviar un WhatsApp al 3884136280 y esperar que se le envíe una pregunta, responder y si lo hacen de manera correcta, pasan a formar parte del grupo de personas que participará del sorteo de premios. La pregunta para competir siempre gira en torno a la temática del Juego Responsable.</p>
+
+
+
+            {/* <p>El material para realizar estas actividades estará disponible en la página <a href="https://inprojuy.gob.ar/" target="_blank" rel="noreferrer noopener">www.inprojuy.gob.ar</a> , en la pestaña que dice “Juego Responsable”. Allí también están disponibles las bases y condiciones.</p> */}
+
+
+
+            <p>El objetivo del certamen es acercar y vincular a los participantes con el número para cualquier situación o pregunta que tengan, siempre con la idea de visibilizar y prevenir el Juego Patológico.</p>
+
+
+
+            <p>Entre los premios se destacan parlantes y auriculares.</p>
+
+
+
+            <p>En tanto, para el Certamen Literario, podrán participar todas las personas que lo deseen y en el caso de menores de edad, deberán adjuntar una autorización firmada por el padre o tutor.</p>
+
+
+
+            <p>El propósito del concurso es la divulgación del Juego Responsable, así como contribuir en la formación y educación de las personas mediante su participación en actividades que demanden investigación, dedicación y esfuerzo.</p>
+
+
+
+            <p>Se valorará especialmente trabajos que ayuden a superar la problemática del trastorno por juego de apuestas, de la misma manera se valorará la originalidad, creatividad, y calidad literaria de los trabajos.</p>
+
+
+
+            <p>El género del escrito será narrativo (relato, cuento, prosa, poética, memoria, carta, diario, etc.)</p>
+
+
+
+            <p>El tribunal que elegirá las obras ganadoras, estará representado por responsables de Salud Mental de la provincia y de la institución organizadora. Los premios serán tablets.</p>
+
+
+
+            <p>Por consultas de bases y condiciones y también el envío de trabajos escribir al mail: juegoresponsable@inprojuy.gob.ar</p>
+
+
+        </div>
+    )
+}
+
+export default NotiAgendame
---
+++ src/paginas/NotiSeminario.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const NotiSeminario = () => {
+    return (
+        <div>
+
+            <h1>Seminario de Juego Responsable WLA CIBELAE, en Río de Janeiro, 22 al 24 de marzo</h1>
+
+            <p>Del 22 al 24 de marzo de 2023, la World Lottery Association (WLA) y la Corporación Iberoamericana de Loterías y Apuestas del Estado (CIBELAE) organizan un seminario sobre Juego Responsable en Río de Janeiro, Brasil.</p>
+
+
+
+            <img src=".\noticias\img\notiseminario.webp" />
+
+
+            <p >El Seminario<strong> Juego Responsable: una conexión transformadora</strong>, con Loterías CAIXA como anfitrión, ofrece a los interesados en la promoción de un juego seguro y responsable en las loterías, la oportunidad de inspirar y orientar en la práctica, desarrollar y amplificar sus programas de Juego Responsable y Responsabilidad Social Corporativa, aportando valor a los operadores, la sociedad y los consumidores.</p>
+
+
+
+            <p >Además, los asistentes tendrán la oportunidad de conectarse con colegas y aprender de entidades que están liderando el campo del juego responsable, realizar networking y generar colaboraciones con otros profesionales.</p>
+
+
+
+            <p >El seminario, que contará con interpretación simultánea en español, portugués e inglés, será en el Windsor Barra Hotel, ubicado en Barra da Tijuca.</p>
+
+
+
+            <p ><strong>Liderazgo femenino</strong></p>
+
+
+
+            <p>En el marco del seminario se celebrará el Primer Encuentro Regional de Mujeres en Loterías, en el que participarán representantes del programa WILL Women in Lottery Leadership.</p>
+
+
+
+            <p>Asimismo, la WLA y CIBELAE celebrarán sus reuniones de Comité Ejecutivo y Junta Directiva, respectivamente.</p>
+
+
+
+            <p>Para <a href="https://cibelae.net/22-24-de-marzo-wla-y-cibelae-organizan-seminario-internacional-de-juego-responsable-en-rio-de-janeiro/"><strong><u>más información</u></strong></a> y ver el programa, ver <a href="https://responsiblegamingseminar.com.br/"><strong><u>aquí</u></strong></a></p>
+
+        </div>
+    )
+}
+
+export default NotiSeminario
