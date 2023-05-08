e484fa31f3c0d533e31526be8edbcdf533224296
Javier <javiteje92@gmail.com>
2023-05-08 04:54:34 -0300

    intento loader

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import loader from './inproloader.gif';
+
+const InproLoader = () => {
+  return (
+    <div className="inproloader">
+      <img src={loader} />
+    </div>
+  );
+};
+
+export default InproLoader;
