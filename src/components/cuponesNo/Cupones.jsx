c217295810e9890b59592313005093bd6626e3f6
Ricardo Tejerina Perezlindo <ricardoperezlindo@gmail.com>
2023-05-15 09:27:04 -0300

    cupones13

--- src/components/cuponesNo/Cupones.jsx
+++ src/components/cuponesNo/Cupones.jsx
@@ -1,49 +1,54 @@
 import React, { useState, useEffect } from "react";
 
-
 function Cupones(props) {
   const [cupones, setCupones] = useState([]);
 
   useEffect(() => {
-    async function getCupones() {
-      const response = await fetch(
-        `https://us-central1-inprojuy-f1173.cloudfunctions.net/app/cupones/${props.id}`
-      );
-      const data = await response.json();
-      setCupones(data);
-    }
+    const getCupones = async () => {
+      try {
+        const response = await fetch(
+          `https://us-central1-inprojuy-f1173.cloudfunctions.net/app/cupones/${props.id}`
+        );
+        const data = await response.json();
+        setCupones(data);
+      } catch (error) {
+        console.log("Error fetching cupones:", error);
+      }
+    };
+
     getCupones();
   }, [props.id]);
 
   return (
     <div className="table-container">
-      {cupones.length === 0 ? (
-        <p>Sin cupones cargados</p>
-      ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Cupón</th>
-              <th>Fecha de carga</th>
-              <th>Número de Participación</th>
-              <th>Mes de sorteo</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cupones.map((cupon, index) => (
+      <table>
+        <thead>
+          <tr>
+            <th>Cupón</th>
+            <th>Fecha de carga</th>
+            <th>Número de Participación</th>
+            <th>Mes de sorteo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(cupones) && cupones.length > 0 ? (
+            cupones.map((cupon, index) => (
               <tr key={index}>
                 <td>{cupon.cupon}</td>
                 <td>{cupon.fechaCarga}</td>
                 <td>{cupon.numParticip}</td>
                 <td>{cupon.mesDeSorteo}</td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+            ))
+          ) : (
+            <tr>
+              <td colSpan={4}>Sin cupones cargados</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
-  
 }
 
 export default Cupones;
