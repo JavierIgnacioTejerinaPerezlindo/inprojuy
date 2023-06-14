ca75eeddff74da581c5bfdb0dae71d3bff68b1e5
Ricardo Tejerina Perezlindo <ricardoperezlindo@gmail.com>
2023-06-14 07:40:18 -0300

    14-06

--- src/paginas/Denuncias.js
+++ src/paginas/Denuncias.js
@@ -4,14 +4,12 @@ const Denuncias = () => {
   return (
     <div className='fondotransparente container'>
         <center><h2 className='botonbordeazul text-white px-5 py-3 mb-4'>Denuncias</h2></center>
-        <p>El juego ilegal es un delito, con penas de prisión de 3 a 6 años.</p>
- <p>El juego oficial sirve a la comunidad. Sus ingresos retornan a la sociedad, convertidos en obras y servicios.</p>
-<p>Ingrese la mayor cantidad posible de datos que contribuyan a identificar dónde se practica el juego clandestino. El sistema de denuncias contra el juego clandestino y reclamos para apostadores ha sido creado para que usted pueda canalizar las denuncias, sugerencias, consultas o quejas, cada vez que lo requiera, asegurando la máxima confidencialidad.</p>
+        <p>Lo invitamos a completar el siguiente formulario ingresando la mayor cantidad posible de datos, y completando los campos obligatorios. </p>
+ <p>El sistema de denuncias contra el juego clandestino del INPROJUY ha sido creado para que usted pueda canalizar las denuncias de manera anónima, asegurando la máxima confidencialidad.</p>
 
           <form className="contactform row g-3 mt-4">
         <div className='col-md-12 m-0 p-0'>
-          <label className="h5 form-label">Apellido y Nombre</label>
-          <input type="text" className=" form-control border px-3 py-2 rounded placeholder-gray-500" />
+         
           <label className="h5 form-label">Correo Electrónico</label>
           <input type="email" className=" form-control border px-3 py-2 rounded placeholder-gray-500" id="exampleInputEmail1" aria-describedby="emailHelp" />
           <label className="h5 form-label">Teléfono</label>
