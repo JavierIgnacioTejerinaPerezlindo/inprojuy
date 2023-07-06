a1e0ff756830c1a247491a549d92442938606a33
Ricardo Tejerina Perezlindo <ricardoperezlindo@gmail.com>
2023-07-06 09:39:06 -0300

    listado de sorteo una más

--- src/paginas/unaMas/CuandoSortea.js
+++ src/paginas/unaMas/CuandoSortea.js
@@ -9,7 +9,7 @@ const CuandoSortea = () => {
         </div>
         <div className='cuerpo'>
         <div className='botonbordeazul mx-auto my-3'>
-                    <h2 className='text-white px-2 py-2 h2 container-fluid'>El sorteo de Cupones No Ganadores del mes de Junio se realizará eL día Viernes 7 de Julio</h2>
+                    <h2 className='text-white px-2 py-2 h2 container-fluid'>El sorteo de Cupones No Ganadores del mes de Junio se realizará el viernes 7 de julio a las 10:15 horas.</h2>
                     
                     </div>
                     <p><a href=".\download\NUMEROSUNAMASSORTEO1.pdf" download >Asignación de numeros para el Sorteo 1</a></p>
